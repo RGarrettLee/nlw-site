dee283fd224c225e80d85ff663e48b54c4d86675
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2025-02-17 17:52:27 -0500

    increased Z-level

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ user }) {
    return (
       <>
-         <div className='flex justify-end mt-5 px-5 sticky top-0'>
+         <div className='flex justify-end mt-5 px-5 sticky top-0 z-10'>
             <ul className='list-none inline'>
                <Link href='/'><li className='hover:text-blue-200 font-inter active:text-blue-100 inline px-2 duration-200 transition-colors'>tiers</li></Link>
                <Link href='/leaderboard'><li className='hover:text-blue-200 font-inter active:text-blue-100 inline px-2 duration-200 transition-colors'>leaderboard</li></Link>

8b1232ed99be61b588fe065f8a767be1d6d6686e
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2025-02-17 05:13:22 -0500

    added gaps between cards

--- pages/submissions.js
+++ pages/submissions.js
@@ -44,17 +44,17 @@ export default function Submissions() {
 
    return (
       <>
-         <div className='grid grid-cols-2 grid-rows-subgrid min-h-screen'>
+         <div className='grid grid-cols-2 grid-rows-subgrid min-w-screen min-h-screen'>
             <div className='flex flex-col items-center justify-center bg-white/5 m-4 rounded-lg gap-2'>
                <h2 className='font-inter text-xl underline underline-offset-2'>Level Submissions</h2>
-               <div className='flex flex-col items-center justify-center'>
+               <div className='flex flex-col items-center justify-center gap-4'>
                   {users?.map((user) => (
                      <>
                         {user?.completions?.map((completion, key) => (
-                           <div key={key} className='flex flex-col items-center justify-center gap-4'>
+                           <div key={key} className='flex flex-col items-center justify-center'>
                               {completion.status === 'pending' ? (
                                  <div className='flex flex-col items-center justify-center gap-2 bg-indigo-700/75 p-4 rounded-lg'>
-                                    <div className='flex items-center max-w-2xl justify-center gap-4'>
+                                    <div className='flex items-center min-w-2xl max-w-2xl justify-center gap-4'>
                                        <div className='flex flex-col items-center justify-center'>
                                           <p className='font-inter text-lg'>{completion.name} by {completion.creators}</p>
                                           <p className='font-inter text-md underline underline-offset-2'>{completion.tier} Tier</p>
@@ -75,6 +75,7 @@ export default function Submissions() {
                                     <div className='flex items-center justify-center gap-4'>
                                        <button onClick={() => approveSubmission(user, key)} className='px-4 py-2 bg-green-700 hover:bg-green-600 active:bg-green-500 duration-200 transition-colors rounded-lg'>Approve</button>
                                        <button onClick={() => denySubmission(user, key)} className='px-4 py-2 bg-red-700 hover:bg-red-600 active:bg-red-500 duration-200 transition-colors rounded-lg'>Deny</button>
+                                       <a href={completion.video} target='_blank' noreferrer='true' className='px-4 py-2 bg-blue-500 hover:bg-blue-400 active:bg-blue-300 duration-200 transition-colors rounded-lg'>Video</a>
                                     </div>
                                  </div>
                               ) : (

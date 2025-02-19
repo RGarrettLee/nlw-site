6663ad346c286ed17a17cd234e8166d4d6382bed
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2025-02-19 05:06:55 -0500

    readded tooltips for tiers

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Disclosure, DisclosureButton, DisclosurePanel, Switch } from '@headlessui/react';
+import { Tooltip } from '@mui/material';
 import LevelSearch from '../components/levelSearch';
 
 export default function Home({ nlwData, user }) {
@@ -35,12 +36,14 @@ export default function Home({ nlwData, user }) {
                     <Disclosure key={key} as='div' className='py-2' defaultOpen={false}>
                       { ({ open }) => (
                         <>
-                          <DisclosureButton className='group flex bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-400 rounded-lg w-fit px-4 py-1 duration-200 transition-colors items-center justify-between gap-1'>
-                            <span className='text-xl font-inter text-white group-data-[hover]:text-white/80'>{tier.name}</span>
-                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-5 ${open ? '' : 'rotate-180 transform'} duration-200 transition-transform`}>
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
-                              </svg>
-                          </DisclosureButton>
+                          <Tooltip title={tier.desc} enterTouchDelay={200} slotProps={{ tooltip: { sx: { userSelect: false, textAlign: 'center', fontSize: '12px' }} }} disableInteractive arrow placement='top'>
+                            <DisclosureButton className='group flex bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-400 rounded-lg w-fit px-4 py-1 duration-200 transition-colors items-center justify-between gap-1'>
+                              <span className='text-xl font-inter text-white group-data-[hover]:text-white/80'>{tier.name}</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-5 ${open ? '' : 'rotate-180 transform'} duration-200 transition-transform`}>
+                                  <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+                                </svg>
+                            </DisclosureButton>
+                          </Tooltip>
                           <DisclosurePanel className='mt-2 text-sm/5 text-white gap-1'>
                               {tier.levels.map((level, index) => (
                                 <button onClick={() => setLevel(level)} key={index} className='text-lg m-0.5 text-start font-inter hover:bg-indigo-600 active:bg-indigo-500 focus:bg-purple-900 duration-200 transition-colors rounded-lg w-fit px-4 py-2'>{level.name}</button>

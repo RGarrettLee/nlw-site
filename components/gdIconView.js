335dd01f8efb36c50a2eeb380cdc6de30a78cf69
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2025-03-19 00:37:55 -0400

    icon size stuff

--- components/gdIconView.js
+++ components/gdIconView.js
@@ -27,14 +27,14 @@ export default function GdIconView({ user }) {
       <>
          {gdInfo?.username ? (
             <div className='flex md:gap-4 items-center justify-center bg-indigo-900/30 px-4 py-2 rounded-lg'>
-               <img src={`https://gdicon.oat.zone/icon.png?type=cube&value=${gdInfo.icon}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-75 md:scale-100' width={iconSize} height={iconSize} alt='cube icon' />
-               <img src={`https://gdicon.oat.zone/icon.png?type=ship&value=${gdInfo.ship}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-75 md:scale-100' width={iconSize + 20} height={iconSize + 20} alt='ship icon' />
-               <img src={`https://gdicon.oat.zone/icon.png?type=ball&value=${gdInfo.ball}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-75 md:scale-100' width={iconSize} height={iconSize} alt='ball icon' />
-               <img src={`https://gdicon.oat.zone/icon.png?type=ufo&value=${gdInfo.ufo}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-75 md:scale-100' width={iconSize} height={iconSize} alt='ufo icon' />
-               <img src={`https://gdicon.oat.zone/icon.png?type=wave&value=${gdInfo.wave}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-75 md:scale-100' width={iconSize} height={iconSize} alt='wave icon' />
-               <img src={`https://gdicon.oat.zone/icon.png?type=robot&value=${gdInfo.robot}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-75 md:scale-100' width={iconSize} height={iconSize} alt='robot icon' />
-               <img src={`https://gdicon.oat.zone/icon.png?type=spider&value=${gdInfo.spider}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-75 md:scale-100' width={iconSize} height={iconSize} alt='spider icon' />
-               <img src={`https://gdicon.oat.zone/icon.png?type=swing&value=${gdInfo.swing}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-75 md:scale-100' width={iconSize} height={iconSize} alt='swing icon' />
+               <img src={`https://gdicon.oat.zone/icon.png?type=cube&value=${gdInfo.icon}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-50 md:scale-100' width={iconSize} height={iconSize} alt='cube icon' />
+               <img src={`https://gdicon.oat.zone/icon.png?type=ship&value=${gdInfo.ship}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-50 md:scale-100' width={iconSize + 20} height={iconSize + 20} alt='ship icon' />
+               <img src={`https://gdicon.oat.zone/icon.png?type=ball&value=${gdInfo.ball}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-50 md:scale-100' width={iconSize} height={iconSize} alt='ball icon' />
+               <img src={`https://gdicon.oat.zone/icon.png?type=ufo&value=${gdInfo.ufo}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-50 md:scale-100' width={iconSize} height={iconSize} alt='ufo icon' />
+               <img src={`https://gdicon.oat.zone/icon.png?type=wave&value=${gdInfo.wave}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-50 md:scale-100' width={iconSize} height={iconSize} alt='wave icon' />
+               <img src={`https://gdicon.oat.zone/icon.png?type=robot&value=${gdInfo.robot}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-50 md:scale-100' width={iconSize} height={iconSize} alt='robot icon' />
+               <img src={`https://gdicon.oat.zone/icon.png?type=spider&value=${gdInfo.spider}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-50 md:scale-100' width={iconSize} height={iconSize} alt='spider icon' />
+               <img src={`https://gdicon.oat.zone/icon.png?type=swing&value=${gdInfo.swing}&color1=${gdInfo.col1}&color2=${gdInfo.col2}`} className='scale-50 md:scale-100' width={iconSize} height={iconSize} alt='swing icon' />
             </div>
          ) : (
             <></>
